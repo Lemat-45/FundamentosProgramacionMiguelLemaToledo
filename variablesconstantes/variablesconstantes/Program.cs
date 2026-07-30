1b0b6f5a607f1d50483cf3d6a6aa6c8045948b72
Lemat-45 <miguel.lema@upb.edu.co>
2026-07-30 08:17:56 -0500

    añadimos el archivo de visual y
    la file cuando se ejecuta dice "bienvenido agente p" está muy chimbita

---
+++ variablesconstantes/variablesconstantes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variablesconstantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "Miguel lema";
+            Console.WriteLine("bienvenido agente P");
+        }
+    }
+}
