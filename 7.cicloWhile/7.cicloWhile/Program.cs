d0042e61b6ad72cb6cd3cf1ec22d9a7f8279a072
Lemat-45 <miguel.lema@upb.edu.co>
2026-08-25 09:39:43 -0500

    MEANWHILE....

---
+++ 7.cicloWhile/7.cicloWhile/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.cicloWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int contador = 1;
+            int acumulador = 1;
+            while (contador < 5)
+            { 
+            contador++;
+            acumulador = acumulador + contador;
+            }
+            Console.WriteLine("la suma de los primeros 5 numeros es " + acumulador);
+            */
+            //crear un algoritmo para que calcule la factorial del numero dado por el usuario
+            int contador = 1;
+            int acumulador = 1;
+            int Numero = 1;
+            Console.WriteLine("inserte un numero para sacar su prima");
+            Numero = int.Parse(Console.ReadLine());
+            while (contador < Numero)
+            {
+                contador++;
+                acumulador = acumulador * contador;
+            }
+            Console.WriteLine(Numero + " factorial es " + acumulador);
+        }
+    }
+}
