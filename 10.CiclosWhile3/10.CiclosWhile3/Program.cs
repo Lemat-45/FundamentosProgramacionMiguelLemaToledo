85b6cae0326c3777d5c775e6251e3d1b1be00104
Lemat-45 <miguel.lema@upb.edu.co>
2026-08-27 09:43:06 -0500

    10 tarea speedrun
    turururu

---
+++ 10.CiclosWhile3/10.CiclosWhile3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.CiclosWhile3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random NumeroPC = new Random();
+            int Numero = NumeroPC.Next(1,101);
+            int NumeroH = -1;
+            Console.WriteLine("Adivina el numero que tengo en mi procesador :3");
+            while (NumeroH != Numero)
+            {
+                NumeroH = int.Parse(Console.ReadLine());
+                if (NumeroH > Numero)
+                {
+                    Console.WriteLine("muy alto! OwO");
+                }
+                if (NumeroH < Numero)
+                {
+                    Console.WriteLine("muy Bajo! UnU");
+                }
+                
+            }
+                Console.WriteLine("Ese era! :O eres mago");
+        }
+    }
+}
