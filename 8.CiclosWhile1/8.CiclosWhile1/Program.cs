06ae9a836bd412907259ecc282eeecb321c6dd50
Lemat-45 <miguel.lema@upb.edu.co>
2026-08-27 09:27:22 -0500

    ciclos while 8 y 9

---
+++ 8.CiclosWhile1/8.CiclosWhile1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.CiclosWhile1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int NUM;
+            NUM = 0;
+            int SUM;
+            SUM = 0;
+            while (NUM >= 0)
+            {
+                Console.WriteLine("ingrese un número positivo para sumarlo, ingrese u numero negativo para cerrar la lista");
+                NUM = int.Parse(Console.ReadLine());
+                SUM = SUM + NUM;
+            }
+            Console.WriteLine("La suma de tus numeros positivos es " + SUM);
+        }
+    }
+}
---
+++ 9.CiclosWhile2/9.CiclosWhile2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.CiclosWilde
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Byte CantNum;
+            Byte CantM0;
+            Byte Cantm0;
+            Byte CantI0;
+            CantM0 = 0;
+            Cantm0 = 0;
+            CantI0 = 0;
+            int Num;
+            Console.WriteLine("Ingrese la cantidad de numeros en su lista");
+            CantNum = Byte.Parse(Console.ReadLine());
+            while (CantNum > 0)
+            {
+                Console.WriteLine("Ingrese el Siguiente Numero " +  CantNum + " numeros restantes" );
+                Num = int.Parse(Console.ReadLine());
+                CantNum--;
+                if (Num == 0)
+                {
+                    CantI0++;
+                }
+                if (Num > 0)
+                {
+                    CantM0++;
+                }
+                if (Num< 0)
+                {
+                    Cantm0++;
+                }
+            }
+            Console.WriteLine("En tu lista hay " + CantI0 + " ceros " + CantM0 + " numeros mayores a cero y " + Cantm0 + " numeros negativos");
+        }
+    }
+}
