2c5849a1887ebb7fccdcac17bb4806a80d5efc6b
Lemat-45 <miguel.lema@upb.edu.co>
2026-09-03 09:51:42 -0500

    for

---
+++ 13 ciclosfor/13 ciclosfor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace _13_ciclosfor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int acumulador=0;
+            //ciclo ford
+            for (int contador=1; contador<=5; contador++)
+            {
+                acumulador += contador;
+            }
+            Console.WriteLine("la suma de los 5 primeros numeros es " + acumulador);
+        }
+    }
+}
---
+++ 13 ciclosfor/13 ciclosfor/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("13 ciclosfor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("13 ciclosfor")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("f500ed68-3db3-42ec-8d8e-5284c6d5932e")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
