030d259708cd6c0b0dc9861ebdb33d4a52849ef6
Lemat-45 <miguel.lema@upb.edu.co>
2026-09-08 09:34:36 -0500

    tashel shiclosh

---
+++ 14.TallerCiclos/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.TallerCiclos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*float contador = 0f; 
+            float puta=0f;
+            bool continuar=false;
+            do
+            {
+                float Calificacion = 0f;
+                contador++;
+                do
+                {
+                    Console.WriteLine("inserte la nota entre 0 a 5");
+                    Calificacion = float.Parse(Console.ReadLine());
+                } while (Calificacion > 5 || Calificacion < 0);
+                puta += Calificacion;
+                Console.WriteLine("quiere añadir más notas? true/false");
+                continuar = bool.Parse(Console.ReadLine());
+            } while (continuar==true);
+            Console.WriteLine("el promedio es " + puta/contador);
+
+            for (int a = 0; a < 5; a++)
+            {
+
+            }*/
+
+        }
+    }
+}
