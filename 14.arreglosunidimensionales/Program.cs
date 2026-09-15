bd0d2e2491ba1879e76db9902f1e161136a94f8a
Lemat-45 <miguel.lema@upb.edu.co>
2026-09-15 09:30:48 -0500

    arreglosuwu

---
+++ 14.arreglosunidimensionales/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.arreglosunidimensionales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int[] numeros = new int [5];
+            numeros[0] = 7;
+            numeros[1] = 6;
+            numeros[2] = 20;
+            numeros[3] = 67;
+            numeros[4] = 76;
+            Console.WriteLine("el dato almacenado en la posicion 4 con indice 3 es: " + numeros[3]);
+            float[] notas = new float[5];
+            notas[0] = 2.9f;
+            notas[1] = 6.7f;
+            notas[2] = 4.4f;
+            notas[3] = 3.2f;
+            notas[4] = 4.3f;
+            char[] simbolos = new char[] { '▓', '♫', 'ᾣ' };
+            string[] nombres = new string[7];
+                for (int i = 0; i<7; i++)
+            {
+                Console.WriteLine("ingrese el nombre para la posición " + (i + 1) + " de indice "+ i);
+                nombres[i] = Console.ReadLine();
+            }
+            for (int i = 0; i < nombres.Length; i++)
+            {
+
+            }
+            */
+            int[] enteros = new int[100];
+            for (int i = 0; i < 100; i++)
+            {
+            enteros[i] = 10;
+            Console.WriteLine(enteros[i]);
+            }
+            
+
+        }
+    }
+}
