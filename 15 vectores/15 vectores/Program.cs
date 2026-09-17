c05a64ed14b407a848d6dccfa05c49f104e5dad0
Lemat-45 <miguel.lema@upb.edu.co>
2026-09-17 09:31:06 -0500

    VECTOR OH YEAHHH

---
+++ 15 vectores/15 vectores/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_vectores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[15];
+            int mayor = 0;
+            int menor = 0;
+            for (int i = 0; i < 15; i++)
+            {
+
+                Console.WriteLine("ingrese su numero deseado para la posición " + (i + 1));
+                numeros[i] = int.Parse(Console.ReadLine());
+                if (i == 0)
+                {
+                    menor = numeros[i];
+                    mayor = numeros[i];
+                }
+
+                if (numeros[i] < menor)
+                {
+                    menor = numeros[i];
+                }
+                else if (numeros[i] > mayor)
+                {
+                    mayor = numeros[i];
+                }
+            }
+            Console.WriteLine("El numero mayor es " + mayor + " y el menor es " + menor);
+        }
+    }
+}
---
+++ Vectortaller/Vectortaller/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace Vectortaller
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int[] numeros = new int [15];
+            int mayor = 0;
+            int menor = 0;
+                for (int i = 0; i < 15; i++)
+            {
+                
+                Console.WriteLine("ingrese su numero deseado para la posición " + (i + 1));
+                numeros[i] = int.Parse(Console.ReadLine());
+                if (i == 0)
+                {
+                    menor = numeros[i];
+                    mayor = numeros[i];
+                }
+
+                if (numeros[i] < menor)
+                {
+                    menor = numeros[i];
+                }
+                else if (numeros[i] > mayor)
+                {
+                    mayor = numeros[i];
+                }
+            }
+            Console.WriteLine("El numero mayor es " + mayor + " y el menor es " + menor);
+
+
+
+            int LVector = int.parse(Console.ReadLine());
+                int[] vector1 = new int[LVector];
+                int[] vector2 = new int[LVector];
+            int contador = 0;
+                for(int i = 0; i < LVector; i++)
+            {
+                Console.WriteLine("ingresa el valor número "+ (i+1) + " del vector 1");
+                vector1[i] = int.parse(Console.readline());
+                Console.WriteLine("ingresa el valor número " + (i + 1) + " del vector 2");
+                vector2[i] = int.parse(Console.readline());
+                if (vector2[i] == vector1[i])
+                {
+                    contador++;
+                    Console.WriteLine("son iguales!")
+                }
+            }
+            Console.WriteLine("Habían " + contador + " coincidencias entre los valores de los dos vectores");
+            */
+
+
+        }
+    }
+}
